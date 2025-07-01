447d811988c23090592cc47408dff92e2e0285d2
michellmics <michell.oliveira1602@gmail.com>
2025-07-01 19:36:14 -0300

    deploy

--- index.php
+++ index.php
@@ -24,15 +24,13 @@
      
       <link rel="preload" href="./css/adminlte.css" as="style">
       <!--end::Accessibility Features--><!--begin::Fonts-->
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fontsource/source-sans-3@5.0.12/index.css" integrity="sha256-tXJfXfp6Ewt1ilPzLDtQnJV4hclT9XuaZUKyUvmyr+Q=" crossorigin="anonymous" media="print" onload="this.media='all'">
+      <link rel="stylesheet" href="./css/index.css" media="print" onload="this.media='all'">
       <!--end::Fonts--><!--begin::Third Party Plugin(OverlayScrollbars)-->
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.11.0/styles/overlayscrollbars.min.css" crossorigin="anonymous">
+      <link rel="stylesheet" href="./css/overlayscrollbars.min.css">
       <!--end::Third Party Plugin(OverlayScrollbars)--><!--begin::Third Party Plugin(Bootstrap Icons)-->
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css" crossorigin="anonymous">
+      <link rel="stylesheet" href="./css/bootstrap-icons.min.css">
       <!--end::Third Party Plugin(Bootstrap Icons)--><!--begin::Required Plugin(AdminLTE)-->
       <link rel="stylesheet" href="./css/adminlte.css">
-      <!--end::Required Plugin(AdminLTE)--><!-- apexcharts -->
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/apexcharts@3.37.1/dist/apexcharts.css" integrity="sha256-4MX+61mt9NVvvuPjUWdUdyfZfxSB1/Rf9WtqRHgG5S0=" crossorigin="anonymous">
    </head>
    <body class="layout-fixed sidebar-expand-lg sidebar-open bg-body-tertiary">
       <!--begin::App Wrapper--> 
@@ -659,14 +657,53 @@ aaa
          </footer>
          <!--end::Footer--> 
       </div>
-      <!--end::App Wrapper--> 
-      <!--begin::Script--> 
-      <!--begin::Third Party Plugin(OverlayScrollbars)-->
-      <script src="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.11.0/browser/overlayscrollbars.browser.es6.min.js" crossorigin="anonymous"></script> <!--end::Third Party Plugin(OverlayScrollbars)--><!--begin::Required Plugin(popperjs for Bootstrap 5)-->
-      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" crossorigin="anonymous"></script> <!--end::Required Plugin(popperjs for Bootstrap 5)--><!--begin::Required Plugin(Bootstrap 5)-->
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.min.js" crossorigin="anonymous"></script> <!--end::Required Plugin(Bootstrap 5)--><!--begin::Required Plugin(AdminLTE)-->
-      <script src="./js/adminlte.js"></script> 
-     
+      <script src="./js/overlayscrollbars.browser.es6.min.js"></script>
+      <script src="./js/popper.min.js"></script>
+      <script src="./js/bootstrap.min.js"></script>
+      <script src="./js/adminlte.js"></script>
+
+      <script>
+        const SELECTOR_SIDEBAR_WRAPPER = ".sidebar-wrapper"
+        const Default = {
+          scrollbarTheme: "os-theme-light",
+          scrollbarAutoHide: "leave",
+          scrollbarClickScroll: true
+        }
+        document.addEventListener("DOMContentLoaded", function () {
+          const sidebarWrapper = document.querySelector(SELECTOR_SIDEBAR_WRAPPER)
+          if (
+            sidebarWrapper &&
+            OverlayScrollbarsGlobal?.OverlayScrollbars !== undefined
+          ) {
+            OverlayScrollbarsGlobal.OverlayScrollbars(sidebarWrapper, {
+              scrollbars: {
+                theme: Default.scrollbarTheme,
+                autoHide: Default.scrollbarAutoHide,
+                clickScroll: Default.scrollbarClickScroll
+              }
+            })
+          }
+        })
+      </script>
+   
+      <script>
+        document.addEventListener('DOMContentLoaded', () => {
+          const cssLink = document.querySelector('link[href*="css/adminlte.css"]');
+          if (!cssLink) return;
+      
+          const cssHref = cssLink.getAttribute('href');
+          const deploymentPath = cssHref.slice(0, cssHref.indexOf('css/adminlte.css'));
+      
+          document.querySelectorAll('img[src^="/assets/"]').forEach(img => {
+            const originalSrc = img.getAttribute('src');
+            if (originalSrc) {
+              const relativeSrc = originalSrc.slice(1);
+              img.src = deploymentPath + relativeSrc;
+            }
+          });
+        });
+      </script>
+
 
    </body>
    <!--end::Body-->
