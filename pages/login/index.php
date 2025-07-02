0f9545e812778eac032594b033daab9aef024b85
michellmics <michell.oliveira1602@gmail.com>
2025-07-01 23:03:58 -0300

    deploy

--- pages/login/index.php
+++ pages/login/index.php
@@ -15,55 +15,55 @@ $siteAdmin = new SITE_ADMIN();
    </head>
    <body class="register-page bg-body-secondary">
       <div class="register-box">
-         <div class="register-logo"> <a href="../index2.html"><b>Admin</b>LTE</a> </div>
+         <div class="register-logo"> <a href="#"><img src="/assets/img/meluza_logo_90.png" alt="Meluza" class="brand-image opacity-75 shadow"></div>
          <!-- /.register-logo --> 
          <div class="card">
             <div class="card-body register-card-body">
-               <p class="register-box-msg">Register a new membership</p>
+               <p class="register-box-msg">Registrar-se</p>
                <form action="../index3.html" method="post">
                   <div class="input-group mb-3">
-                     <input type="text" class="form-control" placeholder="Full Name"> 
+                     <input id="nome" type="text" class="form-control" placeholder="Nome Completo"> 
                      <div class="input-group-text"> <span class="bi bi-person"></span> </div>
                   </div>
                   <div class="input-group mb-3">
-                     <input type="email" class="form-control" placeholder="Email"> 
+                     <input id="email" type="email" class="form-control" placeholder="E-mail"> 
                      <div class="input-group-text"> <span class="bi bi-envelope"></span> </div>
                   </div>
                   <div class="input-group mb-3">
-                     <input type="password" class="form-control" placeholder="Password"> 
+                     <input id="senha" type="password" class="form-control" placeholder="Senha"> 
                      <div class="input-group-text"> <span class="bi bi-lock-fill"></span> </div>
                   </div>
                   <!--begin::Row--> 
                   <div class="row">
                      <div class="col-8">
                         <div class="form-check"> <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"> <label class="form-check-label" for="flexCheckDefault">
-                           I agree to the <a href="#">terms</a> </label> 
+                           Eu aceito os <a href="#">termos</a> </label> 
                         </div>
                      </div>
                      <!-- /.col --> 
                      <div class="col-4">
-                        <div class="d-grid gap-2"> <button type="submit" class="btn btn-primary">Sign In</button> </div>
+                        <div class="d-grid gap-2"> <button type="submit" class="btn btn-primary">Entrar</button> </div>
                      </div>
                      <!-- /.col --> 
                   </div>
                   <!--end::Row--> 
                </form>
                <div class="social-auth-links text-center mb-3 d-grid gap-2">
-                  <p>- OR -</p>
-                  <a href="#" class="btn btn-primary"> <i class="bi bi-facebook me-2"></i> Sign in using Facebook
-                  </a> <a href="#" class="btn btn-danger"> <i class="bi bi-google me-2"></i> Sign in using Google+
+                  <p>- OU -</p>
+                  <a href="#" class="btn btn-primary"> <i class="bi bi-facebook me-2"></i> Entre usando o Facebook
+                  </a> <a href="#" class="btn btn-danger"> <i class="bi bi-google me-2"></i> Entre usando o Google+
                   </a> 
                </div>
                <!-- /.social-auth-links --> 
                <p class="mb-0"> <a href="login.html" class="text-center">
-                  I already have a membership
+                  Eu já sou cadastrado(a)
                   </a> 
                </p>
             </div>
             <!-- /.register-card-body --> 
          </div>
       </div>
-      
+
       <script src="../../js/overlayscrollbars.browser.es6.min.js"></script>
       <script src="../../js/popper.min.js"></script>
       <script src="../../js/bootstrap.min.js"></script>
