338835cac87d3626d1435894d2c3072fa596bfa6
michellmics <michell.oliveira1602@gmail.com>
2025-07-05 12:05:08 -0300

    deploy

--- src/menu.php
+++ src/menu.php
@@ -18,7 +18,7 @@
                </a>
             </li>
             <li class="nav-item">
-               <a href="#" class="nav-link">
+               <a href="/clientes" class="nav-link">
                   <i class="nav-icon mdi mdi-account-multiple"></i>
                   <p>Clientes</p>
                </a>
