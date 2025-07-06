a97cb294cf2e3c22feb6916a1098e15f1a4c0ac0
michellmics <michell.oliveira1602@gmail.com>
2025-07-06 19:38:49 -0300

    deploy

--- pages/clientes/inserClient.php
+++ pages/clientes/inserClient.php
@@ -24,13 +24,13 @@ $siteAdmin = new SITE_ADMIN();
                <div class="container-fluid">
                   <div class="row">
                      <div class="col-sm-6">
-                        <h3 class="mb-0">Página</h3>
+                        <h3 class="mb-0">Clientes</h3>
                      </div>
                      <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-end">
                            <li class="breadcrumb-item"><a href="#">Home</a></li>
                            <li class="breadcrumb-item active" aria-current="page">
-                              Página
+                              
                            </li>
                         </ol>
                      </div>
@@ -45,7 +45,7 @@ $siteAdmin = new SITE_ADMIN();
                             <div class="page-title-box">
                                 <div class="page-title-right">
                                 </div>
-                                <h4 class="page-title">Cadastro de Cliente</h4>
+                                <h4 class="page-title"></h4>
                             </div>
                         </div>
                     </div>
@@ -54,99 +54,64 @@ $siteAdmin = new SITE_ADMIN();
                         <div class="col-lg-6">
                             <div class="card">
                                 <div class="card-body">
-                                    <h4 class="header-title">Novo Usuário</h4>
-                                    <p class="text-muted font-14">Nesta seção você pode cadastrar um novo usuário para ter acesso ao sistema.
+                                    <h4 class="header-title">Novo Cliente</h4>
+                                    <p class="text-muted font-14">Obaa! Cadastre seu novo cliente aqui.
                                     </p>
 
                                     <div class="tab-content">
                                         <div class="tab-pane show active" id="tooltips-validation-preview">
-                                        <form id="form" name="form" role="form" method="POST" enctype="multipart/form-data">
-  
-                                            <div class="position-relative mb-3">
-                                              <label class="form-label" for="email_user">E-mail</label>
-                                              <input id="email_user" name="email_user" pattern="^[^\s@]+@[^\s@]+\.[^\s@]+$" autocomplete="off" spellcheck="false" style="text-transform: uppercase;" type="email" class="form-control" placeholder="" maxlength="50" oninput="this.value = this.value.replace(/[^A-Za-z0-9._@-]/g, '')" required />
-                                            </div>                                          
+                                            <form id="form" name="form" role="form" method="POST" enctype="multipart/form-data">                                       
 
-                                            <div class="position-relative mb-3" id="campo-nome">
-                                              <label class="form-label" for="nome">Nome Completo</label>
-                                              <input id="nome" name="nome" autocomplete="new-nome" style="text-transform: uppercase;" type="text" class="form-control" placeholder="" maxlength="50"  oninput="this.value = this.value.replace(/[^\p{L} ]/gu, '')" required/>
-                                            </div>
-
-                                            <div class="position-relative mb-3" id="campo-data-nascimento">
-                                              <label class="form-label" for="dataNascimento">Data de Nascimento</label>
-                                              <input id="dataNascimento" name="dataNascimento" type="date" class="form-control" required/>
-                                            </div>
-
-                                            <div class="position-relative mb-3" id="campo-sexo">
-                                              <label class="form-label" for="sexo">Sexo</label>
-                                              <select id="sexo" name="sexo" class="form-control" required>
-                                                <option value="">SELECIONE...</option>
-                                                <option value="MASCULINO">MASCULINO</option>
-                                                <option value="FEMININO">FEMININO</option>
-                                                <option value="OUTRO">OUTRO</option>
-                                              </select>
-                                            </div>
-
-                                            <div class="position-relative mb-3">
-                                              <label class="form-label" for="bloco">Bloco</label>
-                                              <input id="bloco" name="bloco" type="text" class="form-control" placeholder="" maxlength="2" required oninput="this.value = this.value.replace(/[^a-zA-Z0-9]/g, '').replace(/^0+/, '');" <?php if(isset($readonly)){echo $readonly;} ?>/>
-                                            </div>
-
-                                            <div class="position-relative mb-3">
-                                              <label class="form-label" for="apartamento">Apartamento</label>
-                                              <input id="apartamento" name="apartamento" type="text" class="form-control" placeholder="" maxlength="4" oninput="this.value = this.value.replace(/[^a-zA-Z0-9]/g, '').replace(/^0+/, '');" required <?php if(isset($readonly)){echo $readonly;} ?>/>
-                                            </div> 
-
-                                            <div class="position-relative mb-3" id="campo-telefone">
-                                              <label class="form-label" for="telefone">DDD + Telefone (Whatsapp)</label>
-                                              <input id="telefone" name="telefone" autocomplete="new-telefone" type="text" class="form-control" placeholder="Ex.: 11982734359" pattern="^\d{11}$" minlength="11" maxlength="11" oninput="this.value = this.value.replace(/[^0-9]/g, '')" required />
-                                            </div> 
-
-                                            <div class="position-relative mb-3" id="campo-senha">
-                                              <label class="form-label" for="senha_user">Senha (mínimo 8 dígitos e ao menos uma letra maiúscula)</label>
-                                              <input id="senha_user" name="senha_user" autocomplete="new-password" type="password" class="form-control" placeholder="" minlength="8" maxlength="12" pattern="^(?=.*[A-Z])(?=.*\d).{8,12}$" title="A senha deve ter no mínimo 8 caracteres e conter pelo menos uma letra maiúscula e um número." required />
-                                            </div>
-
-                                            <div class="position-relative mb-3" id="campo-foto">
-                                              <label class="form-label" for="foto">Foto</label>
-                                              <input id="foto" name="foto" type="file" class="form-control"  accept=".jpg,.jpeg,.png,.gif" />
-                                            </div> 
-
-
-                                              <h6 class="font-15 mt-3">Nível de Acesso</h6>
-                                              <div class="mt-2">                                                      
-                                                <div class="form-check form-check-inline form-radio-info mb-2">
-                                                  <input value="INQUILINO" type="radio" id="funcao1" name="funcao" class="form-check-input" checked required>
-                                                  <label class="form-check-label" for="funcao1">Inquilino</label>
+                                                <div class="position-relative mb-3" id="campo-nome">
+                                                  <label class="form-label" for="nome">Nome Completo</label>
+                                                  <input id="nome" name="nome" autocomplete="new-nome" style="text-transform: uppercase;" type="text" class="form-control" placeholder="" maxlength="50"  oninput="this.value = this.value.replace(/[^\p{L} ]/gu, '')" required/>
                                                 </div>
-                                                <div class="form-check form-check-inline form-radio-danger mb-2">
-                                                  <input value="PROPRIETARIO/MORADOR" type="radio" id="funcao2" name="funcao" class="form-check-input">
-                                                  <label class="form-check-label" for="funcao2">Proprietário/Morador</label>
-                                                </div> 
-                                                <div class="form-check form-check-inline form-radio-danger mb-2">
-                                                  <input value="PROPRIETARIO" type="radio" id="funcao3" name="funcao" class="form-check-input">
-                                                  <label class="form-check-label" for="funcao3">Proprietário</label>
-                                                </div> 
-                                                <div class="form-check form-check-inline form-radio-danger mb-2">
-                                                  <input value="CONSELHEIRO/MORADOR" type="radio" id="funcao4" name="funcao" class="form-check-input">
-                                                  <label class="form-check-label" for="funcao4">Conselheiro/Morador</label>
+
+                                                <div class="position-relative mb-3" id="campo-telefone">
+                                                  <label class="form-label" for="telefone">DDD + Telefone (Whatsapp)</label>
+                                                  <input id="telefone" name="telefone" autocomplete="new-telefone" type="text" class="form-control" placeholder="Ex.: 11982734359" pattern="^\d{11}$" minlength="11" maxlength="11" oninput="this.value = this.value.replace(/[^0-9]/g, '')" required />
                                                 </div> 
 
-                                                
-                                                <div class="form-check form-check-inline form-radio-danger mb-2">
-                                                  <input value="PARCEIRO" type="radio" id="funcao5" name="funcao" class="form-check-input">
-                                                  <label class="form-check-label" for="funcao5">Parceiro Comercial</label>
+                                                <!-- Campos de endereço -->
+                                                <div class="position-relative mb-3">
+                                                    <label for="cep" class="form-label">CEP</label>
+                                                    <input type="text" id="cep" name="cep" class="form-control" data-toggle="input-mask" data-mask-format="00000-000" placeholder="Digite o CEP" maxlength="9" onblur="buscarEndereco()">
+                                                </div>
+
+                                                <div class="position-relative mb-3">
+                                                    <label for="endereco" class="form-label">Endereço</label>
+                                                    <input type="text" id="endereco" name="endereco" class="form-control" style="text-transform: uppercase;" placeholder="" readonly>
+                                                </div>
+
+                                                <div class="position-relative position-relative mb-3">
+                                                    <label for="numero" class="form-label">Número</label>
+                                                    <input type="text" id="numero" name="numero" class="form-control" style="text-transform: uppercase;" placeholder="Digite o número">
+                                                </div>
+
+                                                <div class="position-relative mb-3">
+                                                    <label for="bairro" class="form-label">Bairro</label>
+                                                    <input type="text" id="bairro" name="bairro" class="form-control" style="text-transform: uppercase;" placeholder="" readonly>
+                                                </div>
+
+                                                <div class="position-relative mb-3">
+                                                    <label for="cidade" class="form-label">Cidade</label>
+                                                    <input type="text" id="cidade" name="cidade" class="form-control" style="text-transform: uppercase;" placeholder="" readonly>
+                                                </div>
+
+                                                <div class="position-relative mb-3">
+                                                    <label for="estado" class="form-label">Estado</label>
+                                                    <input type="text" id="estado" name="estado" class="form-control" style="text-transform: uppercase;" placeholder="" readonly>
+                                                </div>
+
+                                                <div class="position-relative mb-3">
+                                                  <label for="observacao" class="form-label">Observações</label>
+                                                  <p class="text-muted font-13">Observações</p>
+                                                  <textarea class="form-control" maxlength="300" rows="3" id="observacao" name="observacao" placeholder=""></textarea>
                                                 </div> 
-
-
-                                              </div>
-
                                             
-                                            <br>
-                                            
-                                            <button class="btn" style="background-color: #8c52ff; color: white;" onclick="window.history.back()" type="button">Voltar</button>         
-                                            <button class="btn" style="background-color: #2be4c6; color: black;" type="submit" id="botao" name="botao">Salvar</button>                                            
-                                          </form>
+                                                <button class="btn" style="background-color: #6e6c72; color: white;" onclick="window.history.back()" type="button">Voltar</button>         
+                                                <button class="btn" style="background-color: #7eda0d; color: black;" type="submit" id="botao" name="botao">Salvar</button>                                            
+                                            </form>
                                         </div> <!-- end preview-->                                        
                                     </div> <!-- end tab-content-->
                                 </div> <!-- end card-body-->
@@ -166,6 +131,34 @@ $siteAdmin = new SITE_ADMIN();
     <script src="../../js/bootstrap.min.js"></script>
     <script src="../../js/adminlte.js"></script>
 	<?php include_once BASE_PATH . "src/config.php"; ?>
+
+    <script>
+        function buscarEndereco() {
+            var cep = document.getElementById('cep').value.replace(/\D/g, '');
+        
+            if (cep.length !== 8) {
+                alert('CEP inválido!');
+                return;
+            }
+        
+            fetch('https://viacep.com.br/ws/' + cep + '/json/')
+            .then(response => response.json())
+            .then(data => {
+                if (data.erro) {
+                    alert('CEP não encontrado!');
+                } else {
+                    document.getElementById('endereco').value = data.logradouro;
+                    document.getElementById('bairro').value = data.bairro;
+                    document.getElementById('cidade').value = data.localidade;
+                    document.getElementById('estado').value = data.uf;
+                }
+            })
+            .catch(error => {
+                console.error('Erro ao buscar o CEP:', error);
+                alert('Erro ao buscar o CEP!');
+            });
+        }
+    </script>
 
     <script>
         function confirmAndSubmit(event) {
