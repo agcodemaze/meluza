114e5eb8284433c2d94c8fdd81f0b87f515863b1
michellmics <michell.oliveira1602@gmail.com>
2025-07-06 11:52:38 -0300

    deploy

--- pages/agenda/index.php
+++ pages/agenda/index.php
@@ -60,13 +60,13 @@ $siteAdmin = new SITE_ADMIN();
                <div class="container-fluid">
                   <div class="row">
                      <div class="col-sm-6">
-                        <h3 class="mb-0">Página</h3>
+                        <h3 class="mb-0">Agenda</h3>
                      </div>
                      <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-end">
-                           <li class="breadcrumb-item"><a href="#">Home</a></li>
+                           <li class="breadcrumb-item"><a href="/inicial">Inicial</a></li>
                            <li class="breadcrumb-item active" aria-current="page">
-                              Página
+                              
                            </li>
                         </ol>
                      </div>
@@ -81,6 +81,53 @@ $siteAdmin = new SITE_ADMIN();
                             <a href="#" class="dropdown-toggle arrow-none card-drop" data-bs-toggle="dropdown" aria-expanded="false"> </a>
                         </div>
                     </div>
+                                            <div class="row">
+                            <div class="col-12 col-sm-6 col-md-3">
+                                <div class="info-box">
+                                    <span class="info-box-icon text-bg-primary shadow-sm"> <i class="bi bi-gear-fill"></i> </span>
+                                    <div class="info-box-content">
+                                        <span class="info-box-text">CPU Traffic</span>
+                                        <span class="info-box-number">
+                                            10
+                                            <small>%</small>
+                                        </span>
+                                    </div>
+                                    <!-- /.info-box-content -->
+                                </div>
+                                <!-- /.info-box -->
+                            </div>
+                            <!-- /.col -->
+                            <div class="col-12 col-sm-6 col-md-3">
+                                <div class="info-box">
+                                    <span class="info-box-icon text-bg-danger shadow-sm"> <i class="bi bi-hand-thumbs-up-fill"></i> </span>
+                                    <div class="info-box-content"><span class="info-box-text">Likes</span> <span class="info-box-number">41,410</span></div>
+                                    <!-- /.info-box-content -->
+                                </div>
+                                <!-- /.info-box -->
+                            </div>
+                            <!-- /.col -->
+                            <!-- fix for small devices only -->
+                            <!-- <div class="clearfix hidden-md-up"></div> -->
+                            <div class="col-12 col-sm-6 col-md-3">
+                                <div class="info-box">
+                                    <span class="info-box-icon text-bg-success shadow-sm"> <i class="bi bi-cart-fill"></i> </span>
+                                    <div class="info-box-content"><span class="info-box-text">Sales</span> <span class="info-box-number">760</span></div>
+                                    <!-- /.info-box-content -->
+                                </div>
+                                <!-- /.info-box -->
+                            </div>
+                            <!-- /.col -->
+                            <div class="col-12 col-sm-6 col-md-3">
+                                <div class="info-box">
+                                    <span class="info-box-icon text-bg-warning shadow-sm"> <i class="bi bi-people-fill"></i> </span>
+                                    <div class="info-box-content"><span class="info-box-text">New Members</span> <span class="info-box-number">2,000</span></div>
+                                    <!-- /.info-box-content -->
+                                </div>
+                                <!-- /.info-box -->
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                        <!-- /.row -->
                     <div class="card-body pt-0">
                         <div class="row">
                             <div class="col-12">
