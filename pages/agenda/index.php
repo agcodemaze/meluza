70f12bf6c3fcf6b2db8e7d6ec584747cf61ac9e9
michellmics <michell.oliveira1602@gmail.com>
2025-07-06 12:11:56 -0300

    deploy

--- pages/agenda/index.php
+++ pages/agenda/index.php
@@ -136,8 +136,6 @@ $siteAdmin = new SITE_ADMIN();
                                     <div class="card-header">
                                         <h3 class="card-title">Serviços</h3>
                                         <div class="card-tools">
-                                            <button type="button" class="btn btn-tool" data-lte-toggle="card-collapse"><i data-lte-icon="expand" class="bi bi-plus-lg"></i> <i data-lte-icon="collapse" class="bi bi-dash-lg"></i></button>
-                                            <button type="button" class="btn btn-tool" data-lte-toggle="card-remove"><i class="bi bi-x-lg"></i></button>
                                         </div>
                                     </div>
                                     <!-- /.card-header -->
@@ -181,12 +179,6 @@ $siteAdmin = new SITE_ADMIN();
                                         </div>
                                     </div>
                                     <!-- /.card-body -->
-                                    <div class="card-footer text-center">
-                                        <a href="javascript:void(0)" class="uppercase">
-                                            View All Products
-                                        </a>
-                                    </div>
-                                    <!-- /.card-footer -->
                                 </div>
                                 <!-- /.card -->
                             </div>
