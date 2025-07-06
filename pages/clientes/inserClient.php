b006a54907be0fce631acb4c23f096d3296b557a
michellmics <michell.oliveira1602@gmail.com>
2025-07-06 20:55:09 -0300

    deploy

--- pages/clientes/inserClient.php
+++ pages/clientes/inserClient.php
@@ -140,7 +140,7 @@ $siteAdmin = new SITE_ADMIN();
     <script>
         $(document).ready(function() {
           var telefoneMask = function(val) {
-            return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-0000';
+            return '(00) 00000-0000';
           };
       
           var telefoneOptions = {
