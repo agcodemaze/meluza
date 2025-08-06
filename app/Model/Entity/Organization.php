03f02af8ae710722051ffa284ae7a62197a1ed28
michellmics <michell.oliveira1602@gmail.com>
2025-08-06 07:56:08 -0300

    deploy

--- app/Model/Entity/Organization.php
+++ app/Model/Entity/Organization.php
@@ -4,6 +4,34 @@ namespace App\Model\Entity;
 
 class Organization {
 
+    /**
+     * Id da Organização
+     * @var integer
+     */
+    public $id = 1;
+
+    /**
+     * Nome da Organização
+     * @var string
+     */
+    public $name = 'Site do mics';
+
+    /**
+     * Site da Organização
+     * @var string
+     */
+    public $site = "https://merluza.com.br";
+
+    /**
+     * Descricao da Organização
+     * @var string
+     */
+    public $description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad 
+    minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
+    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
+    esse cillum dolore eu fugiat nulla pariatur.";
+
 
 
 }
