f4fa97ab79b8f60075c2f8316b5ad949b83f56e9
michellmics <michell.oliveira1602@gmail.com>
2025-08-06 00:44:29 -0300

    deploy

--- src/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="app-footer">
-   <strong>
-   Meluza &copy; <script>document.write(new Date().getFullYear())</script>&nbsp;
-   <a href="https://codemaze.com.br" class="text-decoration-none">Codemaze</a>.
-</footer>
