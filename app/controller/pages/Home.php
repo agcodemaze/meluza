625d799ff462e34f9e6d1f6d010823ad74217225
michellmics <michell.oliveira1602@gmail.com>
2025-08-06 01:38:46 -0300

    deploy

--- app/controller/pages/Home.php
+++ app/controller/pages/Home.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace App\Controller\Pages;
-/**
- * Metodo responsavel por retornar o conteúdo da Home
- * @return string
- */
-
 
 class Home {
+    /**
+    * Metodo responsavel por retornar o conteúdo da Home
+    * @return string
+    */
+
     public static function getHome() {
         return "Olá Mundo";
     }
