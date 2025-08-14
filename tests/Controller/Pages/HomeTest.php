4e160c35aee182bec645b8c9fa2f2b827ea1bf0e
michellmics <michell.oliveira1602@gmail.com>
2025-08-13 22:15:20 -0300

    deploy

--- tests/Controller/Pages/HomeTest.php
+++ tests/Controller/Pages/HomeTest.php
@@ -9,17 +9,11 @@ class HomeTest extends TestCase
 {
     /**
      * @test
-     * Para executar o teste no terminal:
-     * bash -> ./bin/phpunit
-     */
-
-    /**
-     * @test
      * Verifica se o método 'getHome' existe na classe Home.
      */
+
     public function oMetodoGetHomeExisteNaClasseHome()
     {
-        // ... Lógica do seu primeiro teste ...
         $home = new Home();
         $this->assertTrue(method_exists($home, 'getHome'));
     }
@@ -31,18 +25,11 @@ class HomeTest extends TestCase
 
     public function oMetodoGetHomeRetornaHtmlComONomeDaOrganizacao()
     {
-        // 1. Arrange (Preparação)
-        // Você precisa de uma forma de mockar (simular) a classe Organization
-        // e o seu método getPage para que o teste seja focado apenas em 'getHome'
-        
-        // Neste exemplo, vamos apenas verificar a string final
-        $home = new Home();
-        
+        $home = new Home();        
         // 2. Act (Ação)
         $htmlContent = $home->getHome();
-        
-        // 3. Assert (Verificação)
         // Vamos verificar se a string retornada contém o nome da organização
         $this->assertStringContainsString('Site do mics', $htmlContent);
+
     }
 }
