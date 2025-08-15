423f92d5964571c5016e1075786e5ad99ad11b6c
michellmics <michell.oliveira1602@gmail.com>
2025-08-15 00:08:04 -0300

    deploy

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ define('URL','https://cliente.meluza.com.br');
 $obRouter = new Router(URL);
 
 //ROTA HOME
-$obRouter->get('/',[
+$obRouter->get('/A',[
     function(){
         return new Response(200,Home::getHome());
     }
